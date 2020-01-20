32950173f2972533a62f6564eb5e7802e51cbdd7
万古云霄 <zhangpanqin@outlook.com>
2020-01-21 00:01:41 +0800

    build:初始化项目

---
+++ user-center/src/test/java/com/fly/study/usercenter/UserCenterApplicationTests.java
@@ -0,0 +1,15 @@
+package com.fly.study.usercenter;
+
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserCenterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
